fc57a22831f45ba5d7af2371b1e6b39fc93982a7
Kunal Chandra <kunalchandra2506@gmail.com>
2025-08-10 23:58:45 +0530

    Go

---
+++ variables.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+func printtickets() {
+	const conferenceTickets int = 50
+	var remainingTickets uint = 50
+	conferenceName := "Go Conference"
+
+	fmt.Printf("Welcome to %v booking application.\nWe have total of %v tickets and %v are still available.\nGet your tickets here to attend\n", conferenceName, conferenceTickets, remainingTickets)
+
+	var firstName, lastName, email string
+	var userTickets uint
+
+	fmt.Println("Enter Your First Name: ")
+	fmt.Scanln(&firstName)
+
+	fmt.Println("Enter Your Last Name: ")
+	fmt.Scanln(&lastName)
+
+	fmt.Println("Enter Your Email: ")
+	fmt.Scanln(&email)
+
+	fmt.Println("Enter number of tickets: ")
+	fmt.Scanln(&userTickets)
+
+	if userTickets > remainingTickets {
+		fmt.Printf("Sorry, only %v tickets are available.\n", remainingTickets)
+		return
+	}
+
+	remainingTickets -= userTickets
+
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+}
